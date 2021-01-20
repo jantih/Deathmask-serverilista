6f8e3a69633099b386035766ee214052686fde29
jantih <j.tihula@gmail.com>
2021-01-20 21:07:53 +0200

    Add project files.

---
+++ Serverilista/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Serverilista
+{
+    class Program
+    {
+        readonly HttpClient client = new HttpClient();
+        static async Task Main(string[] args)
+        {
+            Program program = new Program();
+            await program.HaeSivu();
+        }
+        private async Task HaeSivu()
+        {
+            // Hae JSON - vastaus sivulta ja tulosta koko mälli
+            string vastaus = await client.GetStringAsync("http://dpmaster.deathmask.net/?game=warfork&json=1");
+            Console.WriteLine(vastaus);
+
+            // Suodata koko paketti osiksi
+            List<Suodata> suodata = JsonConvert.DeserializeObject<List<Suodata>>(vastaus);
+
+            foreach(var osa in suodata)
+            {
+                // suodata tyhjät serverit
+                if (osa.numplayers == 0)
+                {
+                    continue;
+                }
+                // Tulosta jokaisesta serveristä haluttavat tiedot
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine($"{osa.hostname}:\nModi: {osa.gametype}\nKartta: {osa.map}\n" +
+                    $"Players: {osa.numplayers}/{osa.maxplayers}\nBots: {osa.bots}\n");
+                
+                // Tulosta json syvemmän nestauksen tietoja, tässä tapauksessa pelaajat
+                foreach(var plr in osa.players)
+                {
+                    // Suodata pelaajien nimestä turha sälä
+                    plr.name = plr.name
+                        .Replace("&lt;", "")
+                        .Replace("/font&gt;", "")
+                        .Replace("font color*&gt;", "")
+                        .Replace("font color=\"#FFFFFF\"&gt;", "")
+                        .Replace("font color=\"#000000\"&gt;", "")
+                        .Replace("font color=\"#FF0000\"&gt;", "")
+                        .Replace("font color=\"#00FF00\"&gt;", "")
+                        .Replace("font color=\"#0000FF\"&gt;", "")
+                        .Replace("font color=\"#FF00FF\"&gt;", "")
+                        .Replace("font color=\"#FFFF00\"&gt;", "")
+                        .Replace("font color=\"#00FFFF\"&gt;", "");
+
+                    Console.ResetColor();
+                    Console.WriteLine($"Pelaajan nimi: {plr.name}\nPisteet: {plr.score}\nTiimi: {plr.team}\n" +
+                        $"Latenssi: {plr.ping}\n");
+                }
+            }
+        }
+    }
+}
---
+++ Serverilista/Suodata.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace Serverilista
+{
+    class Suodata
+    {
+        // Json suodatus
+        public string hostname { get; set; }
+        public string gametype { get; set; }
+        public string map { get; set; }
+        public int numplayers { get; set; }
+        public int maxplayers { get; set; }
+        public int numspectators { get; set; }
+        public int bots { get; set; }
+        public List<Players> players { get; set; }
+    }
+    public class Players 
+    {
+        public string name { get; set; }
+        public int score { get; set; }
+        public int team { get; set; }
+        public int ping { get; set; }
+    }
+}
+
